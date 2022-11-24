4c85da3b8154a5bde978bd4db28fe107f428ad85
Etxben <sinodejavi@gmail.com>
2022-11-24 10:52:40 +0100

    Modify comments

--- koans/EmptyIsNullOrIssetKoan.php
+++ koans/EmptyIsNullOrIssetKoan.php
@@ -11,7 +11,7 @@ class EmptyIsNullOrIssetKoan extends TestCase
      */
     function testMethodIsNull()
     {
-        //is_null return __ if variable is null
+        //is_null return true if variable is null
         $this->assertEquals(__, is_null(NULL));
         $this->assertEquals(__, is_null(""));
     }
@@ -21,7 +21,7 @@ class EmptyIsNullOrIssetKoan extends TestCase
      */
     function testMethodIsset()
     {
-        //isset return __ if variable exist and it is not null
+        //isset return true if variable exist and it is not null
         $this->assertEquals(__, isset($something));
 
         $array = array('bar' => 1, 'foo' => null);
@@ -37,27 +37,27 @@ class EmptyIsNullOrIssetKoan extends TestCase
      */
     function testMethodEmpty()
     {
-        //Working with numbers
+        //Working with numbers. Returns true if is 0
         $this->assertEquals(__, empty(0));
         $this->assertEquals(__, empty(1));
 
-        //Working with decimals
+        //Working with decimals. Returns true if is 0.0
         $this->assertEquals(__, empty(0.0));
         $this->assertEquals(__, empty(0.1));
 
-        //Working wiht strings
+        //Working wiht strings. Return false if has any character.
         $this->assertEquals(__, empty(""));
         $this->assertEquals(__, empty(" "));
 
-        //Working with booleans
+        //Working with booleans. Return false if is true
         $this->assertEquals(__, empty(false));
         $this->assertEquals(__, empty(true));
 
-        //working with arrays
+        //working with arrays. Return true if the array has any value.
         $this->assertEquals(__, empty(array()));
         $this->assertEquals(__, empty(array(1)));
 
-        //working with objects
+        //working with objects. Return true if value is an instance
         $this->assertEquals(__, empty((object)array()));
         $this->assertEquals(__, empty(null));
         $this->assertEquals(__, empty($something));
